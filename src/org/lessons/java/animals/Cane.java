ea20837d04f9cd53e7adefb3106187ea9fe4c49e
Pat <dmtpatricica@email.com>
2026-04-18 21:28:28 +0200

    cane update

--- src/org/lessons/java/animals/Cane.java
+++ src/org/lessons/java/animals/Cane.java
@@ -4,14 +4,12 @@ public class Cane extends Animale{
     private String razza;
     private String taglia;
     private boolean hasMicrochip;
-    private String colore;
 
-    public Cane(String nome, String genere, String razza, String taglia, boolean hasMicrochip, String colore) {
-        super(nome, genere);
+    public Cane(String nome, String genere, float peso, String colore, String razza, String taglia, boolean hasMicrochip) {
+        super(nome, genere, peso, colore);
         this.razza = razza;
         this.taglia = taglia;
         this.hasMicrochip = hasMicrochip;
-        this.colore = colore;
     }
     
     public String getRazza() {
@@ -37,18 +35,7 @@ public class Cane extends Animale{
     }
 
     public void setHasMicrochip(boolean hasMicrochip) {
-        if (razza != null) {
-            this.hasMicrochip = hasMicrochip;
-        }
-    }
-    public String getColore() {
-        return this.colore;
-    }
-
-    public void setColore(String colore) {
-        if (razza != null) {
-            this.colore = colore;
-        }
+        this.hasMicrochip = hasMicrochip;
     }
     
     public void verso(){
