1ded9c0f349538af0645ee5ed0cd4f851b358908
Pat <dmtpatricica@email.com>
2026-04-18 19:21:57 +0200

    animals aabstract class

--- src/org/lessons/java/animals/Animale.java
+++ src/org/lessons/java/animals/Animale.java
@@ -2,5 +2,38 @@ package org.lessons.java.animals;
 
 public abstract class Animale {
 
-   
+    private String nome;
+    private String genere;
+
+    protected void Animals(String nome, String genere){
+        this.nome = nome;
+        this.genere = genere;
+    }
+
+    protected String getNome() {
+        return this.nome;
+    }
+
+    protected void setNome(String nome) {
+        if (nome != null) {
+            this.nome = nome;
+        }
+    }
+
+    protected String getGenere() {
+        return this.genere;
+    }
+
+    protected void setGenere(String genere) {
+        if (genere != null) {
+            this.genere = genere;
+        }
+    }
+
+    protected void dormi() {
+        System.out.println("zzz...");
+    }
+
+    public abstract void verso();
+    public abstract void mangia();
 }
