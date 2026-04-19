212e4326920a0e2acba109abfb064fe2a3a4f3df
Pat <dmtpatricica@email.com>
2026-04-19 20:22:59 +0200

    main + instance + instances use

---
+++ src/org/lessons/java/animals/Main.java
@@ -0,0 +1,30 @@
+package org.lessons.java.animals;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cane gina = new Cane("Gina", "canide", 7f, "nero", "meticcio", "piccola", true);
+        Passerotto belfagor = new Passerotto("Belfagor", "volatile", 0.300f, "nero", 20f, false);
+        Aquila pina = new Aquila("Pina", "volatile", 3f, "classico", 1000f, false, false);
+        Delfino pinball = new Delfino("Pinball", "cetacceo", 4000f, "grigio", "collo di bottiglia", true);
+
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        gina.verso();
+        System.out.println(gina);
+        gina.dormi();
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        belfagor.verso();
+        System.out.println(belfagor);
+        belfagor.dormi();
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        pina.verso();
+        System.out.println(pina);
+        pina.dormi();
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        pinball.verso();
+        System.out.println(pinball);
+        pinball.dormi();
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        
+    }
+}
